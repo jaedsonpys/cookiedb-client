da72aed44fd777d869649963384f0ef09fb38733
Jaedson Silva <contacanal798@gmail.com>
2022-12-30 14:10:26 -0300

    feat: create CookieDBClient.connect() method to login user

--- cookiedbclient/client.py
+++ cookiedbclient/client.py
@@ -26,3 +26,19 @@ class CookieDBClient(object):
                 raise exceptions.UserAlreadyExistsError(f'Email "{email}" already used')
         else:
             raise exceptions.InvalidRegisterDataError('Username, email and password required')
+
+    def connect(self, email: str, password: str) -> None:
+        if all([email, password]):
+            response = requests.post(self._server_url + '/login', json={
+                'email': email,
+                'password': password
+            })
+
+            if response.status_code == 201:
+                data: dict = response.json()
+                status, token = data.values()
+                self._token = token
+            elif response.status_code == 401:
+                raise exceptions.LoginUnsuccessfulError('Email or password incorrect')
+        else:
+            raise exceptions.InvalidLoginDataError('Email and password required')
