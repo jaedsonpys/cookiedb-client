b827d34afd55df0698967d3add3db4a4df097544
Jaedson Silva <contacanal798@gmail.com>
2022-12-30 13:44:24 -0300

    feat: receive server URL argument in __init__ method

--- cookiedbclient/client.py
+++ cookiedbclient/client.py
@@ -2,6 +2,8 @@ import requests
 
 
 class CookieDBClient(object):
-    def __init__(self):
-        self._token = None
+    def __init__(self, server_url: str):
+        self._server_url = server_url
         self._login_data = {}
+
+        self._token = None
