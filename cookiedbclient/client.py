164e50e5a492455c74ddabce6d0494e1a56991a1
Jaedson Silva <contacanal798@gmail.com>
2022-12-30 13:51:05 -0300

    feat: create register() method to register database users

--- cookiedbclient/client.py
+++ cookiedbclient/client.py
@@ -2,8 +2,20 @@ import requests
 
 
 class CookieDBClient(object):
-    def __init__(self, server_url: str):
+    def __init__(self, server_url: str) -> None:
         self._server_url = server_url
         self._login_data = {}
 
         self._token = None
+
+    def register(self, username: str, email: str, password: str) -> None:
+        response = requests.post(self._server_url + '/register', json={
+            'username': username,
+            'email': email,
+            'password': password
+        })
+
+        if response.status_code == 201:
+            data: dict = response.json()
+            status, token = data.values()
+            self._token = token
