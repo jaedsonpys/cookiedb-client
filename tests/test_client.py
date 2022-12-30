7ce8a6abc23697c88a809869d0527393a417e117
Jaedson Silva <contacanal798@gmail.com>
2022-12-30 16:21:25 -0300

    test: create CookieDBClient instance

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,10 +4,13 @@ import bupytest
 
 sys.path.insert(0, './')
 
+from cookiedbclient import client
+
 
 class TestClient(bupytest.UnitTest):
     def __init__(self):
         super().__init__()
+        self._db = client.CookieDBClient('http://127.0.0.1:5500')
 
 
 if __name__ == '__main__':
