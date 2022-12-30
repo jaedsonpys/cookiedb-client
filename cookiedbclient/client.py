2b533d9a58c50cae90ec442f88dc26879f7dc373
Jaedson Silva <contacanal798@gmail.com>
2022-12-30 14:01:49 -0300

    feat: check the register data in register() method

--- cookiedbclient/client.py
+++ cookiedbclient/client.py
@@ -1,5 +1,7 @@
 import requests
 
+from . import exceptions
+
 
 class CookieDBClient(object):
     def __init__(self, server_url: str) -> None:
@@ -9,13 +11,16 @@ class CookieDBClient(object):
         self._token = None
 
     def register(self, username: str, email: str, password: str) -> None:
-        response = requests.post(self._server_url + '/register', json={
-            'username': username,
-            'email': email,
-            'password': password
-        })
+        if all([username, email, password]):
+            response = requests.post(self._server_url + '/register', json={
+                'username': username,
+                'email': email,
+                'password': password
+            })
 
-        if response.status_code == 201:
-            data: dict = response.json()
-            status, token = data.values()
-            self._token = token
+            if response.status_code == 201:
+                data: dict = response.json()
+                status, token = data.values()
+                self._token = token
+        else:
+            raise exceptions.InvalidRegisterDataError('Username, email and password required')
