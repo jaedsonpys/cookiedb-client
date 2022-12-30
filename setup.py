4efc126a6474cc6dbd8c072a1bde6b946d4924ac
Jaedson Silva <contacanal798@gmail.com>
2022-12-30 13:27:32 -0300

    create default setup script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='cookiedb-client',
+    description='A client for manipulating CookieDB Server databases.',
+    author='Jaedson Silva',
+    author_email='imunknowuser@protonmail.com'
+)
