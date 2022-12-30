4266593b08e0556a9d44199f893ff60beb3c69dd
Jaedson Silva <contacanal798@gmail.com>
2022-12-30 18:25:03 -0300

    test: create test to CookieDBClient.create_database() method

--- tests/test_client.py
+++ tests/test_client.py
@@ -18,6 +18,8 @@ class TestClient(bupytest.UnitTest):
         self._user_email = f'user{str(random.randint(1000, 9999))}@mail.com'
         self._user_password = str(random.randint(10000, 99999))
 
+        self._database = 'TestDatabase'
+
     def test_register_user(self):
         self.assert_false(self.db.register(self._user_name, self._user_email, self._user_password))
 
@@ -39,6 +41,17 @@ class TestClient(bupytest.UnitTest):
             self.assert_true(True)
         else:
             self.assert_true(False, message='Invalid password not raise exception')
+
+    def test_create_database(self):
+        self.db.create_database(self._database)
+
+    def test_create_same_database(self):
+        try:
+            self.db.create_database(self._database)
+        except exceptions.DatabaseExistsError:
+            self.assert_true(True)
+        else:
+            self.assert_true(False, message='DatabaseExistsError exception not thrown')
 
 
 if __name__ == '__main__':
