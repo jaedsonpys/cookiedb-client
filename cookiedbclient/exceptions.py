28b3e074af0e6405c9f23cc04de2ca3f98db7b43
Jaedson Silva <contacanal798@gmail.com>
2022-12-30 14:09:37 -0300

    feat: create InvalidLoginDataError and LoginUnsuccessfulError exceptions

--- cookiedbclient/exceptions.py
+++ cookiedbclient/exceptions.py
@@ -6,3 +6,13 @@ class UserAlreadyExistsError(Exception):
 class InvalidRegisterDataError(Exception):
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
+
+
+class InvalidLoginDataError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+class LoginUnsuccessfulError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
