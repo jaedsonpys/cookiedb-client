433e8a90409e7b34eb895aef8795a66b01174bfe
Jaedson Silva <contacanal798@gmail.com>
2022-12-31 14:40:52 -0300

    build: add long description to package

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open('README.md', 'r') as reader:
+    readme = reader.read()
+
 setup(
     name='cookiedb-client',
     description='A client for manipulating CookieDB Server databases.',
+    long_description=readme,
+    long_description_content_type='text/markdown'
     version='0.1.0',
     author='Jaedson Silva',
     author_email='imunknowuser@protonmail.com',
