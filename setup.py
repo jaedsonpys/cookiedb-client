1924a60a27d9d6eae1e611ea19bb88a26642a7bf
Jaedson Silva <contacanal798@gmail.com>
2022-12-31 14:41:13 -0300

    build: fix common in arguments

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='cookiedb-client',
     description='A client for manipulating CookieDB Server databases.',
     long_description=readme,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
     version='0.1.0',
     author='Jaedson Silva',
     author_email='imunknowuser@protonmail.com',
