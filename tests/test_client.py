74e6b3de331fd3eaca51885fb2084872211dfc4e
Jaedson Silva <contacanal798@gmail.com>
2023-01-16 21:30:24 -0300

    update test to socket use version

--- tests/test_client.py
+++ tests/test_client.py
@@ -12,11 +12,8 @@ class TestClient(bupytest.UnitTest):
     def __init__(self):
         super().__init__()
 
-        self.db = client.CookieDBClient('http://127.0.0.1:5500')
-
-        self._user_name = 'TestClient'
-        self._user_email = f'user{str(random.randint(1000, 9999))}@mail.com'
-        self._user_password = str(random.randint(10000, 99999))
+        self.db = client.CookieDBClient()
+        self.db.connect('127.0.0.1', '12345678')
 
         self._database = 'TestDatabase'
 
@@ -30,28 +27,6 @@ class TestClient(bupytest.UnitTest):
                 'interpreted': False
             }
         }
-
-    def test_register_user(self):
-        self.assert_false(self.db.register(self._user_name, self._user_email, self._user_password))
-
-    def test_register_same_user(self):
-        try:
-            self.db.register(self._user_name, self._user_email, self._user_password)
-        except exceptions.UserAlreadyExistsError:
-            self.assert_true(True)
-        else:
-            self.assert_true(False, message='Existing user registration does not return exception')
-
-    def test_connect_user(self):
-        self.assert_false(self.db.login(self._user_email, self._user_password))
-
-    def test_connect_user_with_invalid_password(self):
-        try:
-            self.db.login(self._user_email, 'random-password')
-        except exceptions.LoginUnsuccessfulError:
-            self.assert_true(True)
-        else:
-            self.assert_true(False, message='Invalid password not raise exception')
 
     def test_create_database(self):
         self.db.create_database(self._database)
